86e513ac551b784ebc40d3ddaa89906158830570
Bilal Hassan <52100542+bilal-micro@users.noreply.github.com>
2025-04-30 17:04:30 +0300

    Create S1.cs

---
+++ Two Sum/S1.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+            if (!map.ContainsKey(nums[i])) {
+                map[nums[i]] = i;
+            }
+        }
+        return null;
+    }
+}
